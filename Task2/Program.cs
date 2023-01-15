08d5f81028076741399fecce82725863f87848ed
Ne1epost <ne1epost@me.com>
2023-01-15 13:52:34 +0300

    ДЗ 2
    1

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Принимает на вход число и выдает сумму цифр в числе
+
+Console.Write("Введите число: ");
+string value = Console.ReadLine();
+int sum = 0;
+
+for (int i =0; i < value.Length; i++)
+{
+    int number = int.Parse(Convert.ToString(value[i]));
+    sum += number;
+}
+Console.Write(sum);
