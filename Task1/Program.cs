682a5fdb1c3fab82af411f65524b8da82735b725
Ne1epost <ne1epost@me.com>
2023-01-15 13:51:39 +0300

    ДЗ

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 25 Напишите цикл который принимает 2 числа и возводит в степень
+
+Console.WriteLine("Введите значение A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение B(степень в котору возводим): ");
+int numB = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+
+for (int i =0; i < numB; i++)
+    {
+        result = result * numA;
+    }
+Console.Write(result);
+
+
+
